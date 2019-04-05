1ce93ac80ff21fd1b6d24aa99225b110ef6a8b2f
Peter Hasik <pbhasik@email.uark.edu>
2019-04-05 12:36:44 -0500

    added in GFF and FSA parsing arguments 4/5/2019

--- parsegff.py
+++ parsegff.py
@@ -1,4 +1,22 @@
-#! /usr/bin/python
+#!/usr/bin/env python3
+
+
+import argparse
+
+# create an ArgumentParser object ('parser') that will hold all the information necessary to parse the command line
+parser = argparse.ArgumentParser(description = "generates a parse script for FSA and GFF files")
+
+# add positional arguments, i.e. the required input, so whatever I put after the write_trestles_PBS.py will become the job name
+parser.add_argument("fsa_file", help= "The name of the FSA file" )
+parser.add_argument("gff_file", help= "The name of the GFF file" )
+
+
+# parse the command line arguments
+args = parser.parse_args()
+
+# creating args for the file names
+fsa_file = args.fsa_file
+gff_file = args.gff_file
 
 # Opening and storing FASTA file as a list, fastalines = genome of qatermelon
 with open("watermelon.fsa") as w:
@@ -23,15 +41,15 @@ def get_gc_content(substring):
 
 # parse the files and extract the genome features 
 
-startsandstops = []
+#startandstops = []
 
 for line in gff:
     line = line.rstrip('\n')
     fields = line.split('\t')
-    start = int(fields[3]) - 1
-    stop = int(fields[4]) - 1
+    start = (int(fields[3]) - 1)
+    stop = (int(fields[4]) - 1)
     output = fastalines[start:stop]
-    startsandstops.append(output)
+    #startsandstops.append(output)
     print(get_gc_content(output))
     
 gff.close()
